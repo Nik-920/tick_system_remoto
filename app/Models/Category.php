4ee3ed221c6077632b387251b70981f803a54c88
Nik-920 <2002010188@undc.edu.pe>
2026-04-16 20:38:55 -0500

    Comentario Enviar AL github

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-// CATEGORIAS ENVIAR
+// CATEGORIAS ENVIAR AL GITHUB
 class Category extends Model
 {
     use HasFactory, HasUuids;
