2aa713f580d952c693b229f380ca42ded1fb5a62
Nik-920 <2002010188@undc.edu.pe>
2026-04-16 20:45:35 -0500

    Comentario

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-// CATEGORIAS ENVIAR AL GITHUB PARA QUE SEAN USADAS EN EL FRONTEND
+// CATEGORIAS COMENTARIO
 class Category extends Model
 {
     use HasFactory, HasUuids;
