dfa17d16b60bfe627e56eb722c1f0d82597b232a
Nik-920 <2002010188@undc.edu.pe>
2026-05-25 19:54:50 -0500

    feat: agregar configuración para el número máximo de candidatos en la detección de ubicaciones similares

--- config/locations.php
+++ config/locations.php
@@ -4,4 +4,5 @@ return [
     'duplicate_detection_enabled' => env('LOCATION_DUPLICATE_DETECTION_ENABLED', true),
     'similarity_threshold' => env('LOCATION_SIMILARITY_THRESHOLD', 0.75),
     'duplicate_active_only' => env('LOCATION_DUPLICATE_ACTIVE_ONLY', true),
+    'max_candidates' => (int) env('LOCATION_SIMILARITY_MAX_CANDIDATES', 100),
 ];
