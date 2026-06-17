50e04d19bce5bc8e06db8b0cce179d03ce0a78f9
Nik-920 <2002010188@undc.edu.pe>
2026-06-16 23:10:44 -0500

    feat: add dashboard cards, CSS modules for locations, and auto-merge workflow configuration

--- resources/views/locations/partials/index/top-incidents-card.blade.php
+++ resources/views/locations/partials/index/top-incidents-card.blade.php
@@ -2,19 +2,21 @@
 <div class="loc-aside-card">
     <h2 class="loc-aside-card__title">Top ubicaciones con más incidencias</h2>
 
-    @if (count($topIncidents) > 0)
-        <ul class="loc-top-list" role="list">
-            @foreach ($topIncidents as $item)
-                <li class="loc-top-list__item">
-                    <span class="loc-top-list__meta">
-                        <span class="loc-top-list__code">{{ $item['code'] }}</span>
-                        <span class="loc-top-list__name">{{ $item['name'] }}</span>
-                    </span>
-                    <span class="loc-top-list__count {{ $item['color'] }}">{{ $item['count'] }}</span>
-                </li>
-            @endforeach
-        </ul>
-    @else
-        <p class="loc-aside-card__empty">Sin incidencias registradas aún.</p>
-    @endif
+    <div class="loc-aside-card__body">
+        @if (count($topIncidents) > 0)
+            <ul class="loc-top-list" role="list">
+                @foreach ($topIncidents as $item)
+                    <li class="loc-top-list__item">
+                        <span class="loc-top-list__meta">
+                            <span class="loc-top-list__code">{{ $item['code'] }}</span>
+                            <span class="loc-top-list__name">{{ $item['name'] }}</span>
+                        </span>
+                        <span class="loc-top-list__count {{ $item['color'] }}">{{ $item['count'] }}</span>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p class="loc-aside-card__empty">Sin incidencias registradas aún.</p>
+        @endif
+    </div>
 </div>
